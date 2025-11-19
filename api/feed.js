284ea33821047b435b3f369876fedf86017b22df
feb716 <sahidinf@gmail.com>
2025-11-19 14:32:28 +0700

    Update feed.js

--- api/feed.js
+++ api/feed.js
@@ -61,7 +61,6 @@ module.exports = async (req, res) => {
         <html lang="en">
         <head>
             <meta charset="UTF-8">
-            <meta name="monetag" content="1b949bb2ec11c7b38b30da69aacf4401">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>${siteTitle}</title>
             <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
